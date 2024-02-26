b11fc89754d603f4b3d794a950b15d9af8e2ba04
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-02-25 16:19:17 -0930

    jsAssignments

---
+++ assignment three/app.js
@@ -0,0 +1,13 @@
+var age=(18)
+alert("i am"+age +" years old"   )
+var visitwebsite=(14)
+alert("you have visit " + visitwebsite +" time")
+var birthyear=(2006)
+document.write( "my birth year is "  + birthyear +"<br>" + "the variable is stored in number"   )
+
+var visitorname=( "Raffay" )
+var producttitle=("t-shirts")
+var quantity=(4)
+document.write( "<br>" + visitorname + " ordered " +  quantity + producttitle + " on abc store ")
+
+
