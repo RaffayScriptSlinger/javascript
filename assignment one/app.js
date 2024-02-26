f47283ac83b08bdf2961f7fb5853e52471918ac5
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-02-25 15:43:27 -0930

    Initial commit

---
+++ assignment one/app.js
@@ -0,0 +1,40 @@
+// // alert("Hello world");
+// // data ke 8 types han
+// //1.string jo single quate ya double me aye eg: "hellow "  , `hellow`
+// //2.number ho or single quote or double code me na ho eg:10,20
+// //3.Bolean true or false
+// //4.undefined 
+// //5.function
+// // 6.object
+// //7.arrray
+// //8.null
+
+// //variable banane ka tareeqa
+// // var ka keyword lekenge phir space deke variable ka nam leekhenge jo data save horaha han uske hisab se phir equal to ka sign (=) value dedege
+// var greetings ="hellow world"
+// var noofstudents=50
+// var classisgoing=true
+// var noofstudentspassed
+
+
+// alert(greetings)
+// alert(noofstudents)
+// alert(classisgoing)
+// alert(noofstudentspassed)
+// //variable rules
+// // 1 variable number se start nahe hosakta han eg: var 1st
+// // 2. variable ka name javascript ke statements ke jese nahe rakh sakte jese if,else,else if
+// //3.variable ka name me underscroe  or dollar ke sign ke elawa or koi special character nahe desakte
+// //4.variable store karwane ke best practice ya ha ke variable ko camelcase me leeekha jaye
+// //5.variable me gap nahe desakte
+
+
+// console.log("hello world");
+// document.write(greetings)
+// var raffay=("smit")
+// console.log(raffay);
+ alert("Error please enter a valid password" )
+ alert("Welcome to js world \n happy coding")
+ window.alert("Welcome to js land")
+ document.write("Happy coding")
+ console.log("hellow i can run js through my web browsers console"  )
