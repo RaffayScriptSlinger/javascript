cc8227d0c457ca9d4a2352b06b92d92cac147c10
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-02-28 08:56:10 -0930

    chapter 4 upgrated

---
+++ assignment four/app.js
@@ -0,0 +1,26 @@
+// question number one
+var name="Raffay"
+var  age="17"
+var  institude="saylani mass it trainning"
+console.log( "my name is " +  name + " my age is "+ age +" currently studying in " + institude    )
+// question number two
+// Legal Variable
+var name=("Raffay");
+var age=("17")
+var status=("single")
+var fathername=("Sharjeel")
+var education=("Intermediate")
+// illegal variable
+// var var=("abc")
+// var 1=("qqq")
+// var %=("Rs")
+// var 1st=("raffay")
+// var 5th=("five")
+// Question three
+   
+
+//question three
+document.write(  " <br>   Rules OF Declaring Variable "  )
+document.write( "<br>   Variable names can only contain  Number , $  and for example $my-1stVariable")
+document.write( "<br>  Variable names are case sensitive" )
+document.write( " <br> Variable name should not be Js")
