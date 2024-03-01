a3ad6fba86b57d523f53d6df57b0483d680f4ac4
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-03-01 13:00:47 -0930

    Chapter 5,6 Updated

---
+++ Assignment five/app.js
@@ -0,0 +1,161 @@
+// Question One
+document.write( "Question nmber one <br>")
+var a = 12;
+var b = 6;
+document.write(a + b);
+
+document.write("<br>  <br>");
+document.write("question number Two")
+document.write("<br>");
+// Question Two
+var c = 5;
+var d = 2;
+document.write(c - d);
+
+document.write("<br>");
+var e =(6);
+var f =(2);
+document.write(e * f);
+
+document.write("<br>");
+var raffay=(3)
+var sharjeel=(3)
+document.write(raffay / sharjeel)
+
+ // Question number three
+ document.write("<br>");
+ document.write("question number Three")
+ document.write("<br>");
+  var number = (5)
+  document.write(number++);
+  document.write("<br>")
+  document.write(number);
+var myVariable;
+
+
+document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+
+myVariable = 5;
+
+
+document.write("Initial value: " + myVariable + "<br>");
+
+
+
+myVariable++;
+
+
+document.write("Value after increment is: " + myVariable + "<br>");
+
+
+myVariable += 7;
+
+
+document.write("Value after addition is: " + myVariable + "<br>");
+
+
+myVariable--;
+
+
+document.write("Value after decrement is: " + myVariable + "<br>");
+
+
+var remainder = myVariable % 3;
+
+
+document.write("The remainder is: " + remainder + "<br>" + "<br>");
+
+// Question number 4
+document.write("Question number four  <br>  <br>" )
+var cost=(600)
+document.write( "Total cost of buying five tickets is " + cost*5 + "<br> <br>   " )
+// Question number 5
+document.write("Question number 5 <br>    <br>")
+var Table =( 4 );
+// document.write( "Table of four" )
+document.write( Table + "<br>")
+Table=Table +4
+document.write( Table + "<br>")
+Table=Table + 4
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br>")
+Table=Table +4 
+document.write( Table + "<br> <br> ")
+Table=Table +4 
+// Question Number 7
+
+document.write("Question number 7 <br>    <br>")
+var priceOfItem1=650;
+var priceOfItem2=100;
+var quantityOfItem1=3;
+var quantityOfItem2=7;
+var shippingCharges=100;
+ 
+document.write("Price of item is " +  priceOfItem1 + "<br> ")
+
+document.write("Quantity of item 1 is " + quantityOfItem1 + "<br> ")
+document.write( "Price of item 2 is" + priceOfItem2 + "<br>" )
+
+document.write("Quantity of item 2 is " + quantityOfItem2 + "<br> ")
+document.write(" shipping charges " + shippingCharges + "<br>" )
+
+document.write("Total cost of Your Order is " + (priceOfItem1  *  quantityOfItem1 +  priceOfItem2 * 7 + shippingCharges) +" <br> <br> " )
+
+// Question number 8
+document.write("Question number 8  <br> " )
+var totalNumber= 980;
+var obtainNumber=804;
+document.write( obtainNumber / totalNumber *100  + "<br>  <br>" )
+// Question number 8 ended
+
+// Question number 9 is started
+document.write("Question number 9 <br>")
+var dollars=104.80;
+var riyal=28;
+console.log(dollars * 10 )
+console.log(riyal * 25 )
+document.write(dollars * 10 + riyal * 25 + "<br> <br>") 
+
+// Question number 10
+document.write("Qustion number 10 <br>")
+var number=5;
+document.write(number + 5 *10 /2 +"<br> <br>")
+
+// Question Numeber 11
+document.write("Question number 11" +" <br>")
+var currentYear = 2024;
+var  birthYear= 2006;
+document.write(currentYear-birthYear + "<br> <br>" )
+
+document.write("Question number 12" +" <br>")
+
+// Question no 12 Skipped
+
+// Question number 13
+
+// var favouriteSnack="ChoclateChip"
+// var currentage=15
+// var maximumAge=65
+// var amountOfSnackPerDay=3
+// document.write(maximumAge-currentage)
+// console.log (  maximumAge - currentage + amountOfSnackPerDay  )
+var favouriteSnack = "ChocolateChip";
+var currentAge = 15;
+var maximumAge = 65;
+var amountOfSnackPerDay = 3;
+
+// document.write(maximumAge - currentAge);
+document.write( (maximumAge - currentAge) * amountOfSnackPerDay)
+
---
+++ assignment six/app.js
@@ -0,0 +1,27 @@
+// Question number one
+document.write("Question Number one started  <br>")
+
+var a = 10;
+document.write(++a +"<br>")
+document.write(a++ +"<br>" )
+document.write(a +"<br>")
+document.write(--a +"<br>")
+document.write(--a +"<br>")
+// Question two
+document.write("Question Number two  started  <br>")
+var a=2;
+var b=1;
+document.write(--a +"<br>") ;
+document.write(-a - --b +"<br>");
+document.write(-a - --b + ++b +"<br>");
+document.write(-a - --b + ++b + b-- +"<br>");
+//Question Three
+var username = prompt("Enter your Name:");
+if (username !==null){
+    alert("hello , " + username + " Welcome ");
+
+}
+else{
+   alert ( "Hello! Welcome!")
+}
+// QUESTION  5 is skipped
