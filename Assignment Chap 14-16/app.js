20faf0329f93ccc3741fada6959f1f44430000a5
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-03-16 13:01:42 -0930

    Chapter 14 & 16 Assignments Complete !

---
+++ Assignment Chap 14-16/app.js
@@ -0,0 +1,68 @@
+
+
+// Question Number One
+
+var studentName = []
+console.log(studentName)
+  
+
+// Question Number Two
+
+studentName.push("Raffay" , "Sharjeel" , "Rehan")
+console.log(studentName);
+
+
+// Question Number Three
+
+let stringArray = [
+    "stringOne", "stringTwo" , "stringThree"  
+]
+console.log(stringArray);
+
+// Question Number Four
+
+let numberArrays = [
+    1 , 2 ,5 , 4
+]
+console.log(numberArrays)
+
+// Question Number Five
+
+let booleanArray = [
+    true , false
+]
+
+console.log(booleanArray)
+
+// Question Number Six
+
+let mixArray = [
+    "Raffay" , 45 , true
+]
+console.log(mixArray)
+
+// Question Number Seven
+
+let qualifications = [
+    "SSC", "HSC" , "BSC" , "BS" , "BCOM" , "MS" , "M.PHIL" , "PHD"
+];
+console.log(qualifications)
+
+// Question Number Eight
+
+// skipped
+
+// Question Number Nine
+
+let color =  [ "pink , Purple"];
+
+let colorOne = prompt("what color You wants to add to the beginning");
+
+color.unshift(colorOne);
+console.log(color);
+
+let colorTwo = prompt("what color You wants to add to the End");
+
+color.push(colorTwo);
+
+console.log(color);
